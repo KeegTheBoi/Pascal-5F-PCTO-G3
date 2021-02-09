b8fcf108312e705d449f732c78b2f636d8bb7ff7
Keegan Carlo Falcao <KEEGAN CARLO.FALCAO@local.ittcesena.it>
2021-02-09 09:27:21 +0100

    versione con cartella Visual Studio

---
+++ ServerGestioneMagazzino/ServerGestioneMagazzino/Server.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerGestioneMagazzino
+{
+    class Server
+    {
+        bool _onProcess = true;
+        List<Thread> _listaClientThread;
+        public Server(int port, IPAddress ip)
+        {
+            EndPoint = new IPEndPoint(ip, port);
+            IndirizzoIP = ip;
+        }
+
+        public IPEndPoint EndPoint { get; set; }
+
+        public IPAddress IndirizzoIP { get; set; }
+
+        //Socket del seerver
+        public Socket Ascoltatore { get; set; }
+
+
+        /// <summary>
+        /// Inizializza il socket del server
+        /// </summary>
+        public void Initialize()
+        {
+            Ascoltatore = new Socket(IndirizzoIP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Ascoltatore.Bind(EndPoint);
+        }
+
+        /// <summary>
+        /// //Il server si mette ad ascoltare 
+        /// </summary>
+        public void StartListening()
+        {
+            //Il server si mette ad ascoltare 
+            Ascoltatore.Listen(10);
+            _listaClientThread = new List<Thread>();
+            //parte un thread dove viene gestita la ricezione
+            Accept();
+        }
+
+        /// <summary>
+        /// Il socket del server accetta una connessione dal client
+        /// </summary>
+        public void Accept()
+        {
+            //Esegue il ciclo mentre il server è attivo
+            while (_onProcess)
+            {                
+                Socket clientSock = Ascoltatore.Accept();
+                Client client = new Client(clientSock);
+                Thread clientThread = new Thread(client.Ricezione);
+                clientThread.Start();
+                _listaClientThread.Add(clientThread);                               
+            }
+        }
+
+        /// <summary>
+        /// Chiude il server
+        /// </summary>
+        public void EndListening()
+        {
+            foreach(Thread t in _listaClientThread)
+            {
+                t.Abort();
+                t.Join();
+            }
+        }
+
+
+
+    }
+}
