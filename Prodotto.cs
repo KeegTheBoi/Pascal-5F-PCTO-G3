4233e20c690343be85f3bc5ec93eba734c6dbd2e
Keegan Carlo Falcao <KEEGAN CARLO.FALCAO@local.ittcesena.it>
2021-02-23 09:12:03 +0100

    files

---
+++ Prodotto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerGestioneMagazzino
+{
+    class Prodotto
+    {
+        public int CodiceOperazione { get; set; }
+
+        public int CodiceRichiesta { get; set; }
+
+        public int Quantità { get; set; }
+
+        public int CodiceProdotto { get; set; }
+    }
+}
