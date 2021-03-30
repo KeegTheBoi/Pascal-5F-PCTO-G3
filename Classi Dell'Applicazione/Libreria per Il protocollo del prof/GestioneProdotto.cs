708f9906e04487c137e551a688806b2239fc0c35
Keegan Carlo Falcao <64664376+KeegTheBoi@users.noreply.github.com>
2021-03-30 08:58:52 +0200

    Add files via upload

---
+++ Classi Dell'Applicazione/Libreria per Il protocollo del prof/GestioneProdotto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProtocolloGenerale
+{
+    class GestioneProdotto : Operazione
+    {
+        public GestioneProdotto(int cod, string nome, int quantita, string codProd) : base (cod)
+        {
+            NomeProdotto = nome;
+            QuantitaProdotti = quantita;
+            IDProdotto = codProd;
+        }
+
+        public string IDProdotto { get; private set; }
+        public string NomeProdotto { get; set; }
+        public int QuantitaProdotti { get; private set; }
+    }
+}
