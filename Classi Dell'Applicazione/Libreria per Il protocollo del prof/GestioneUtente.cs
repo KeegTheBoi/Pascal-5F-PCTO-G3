9a5f1473856ea027ccc1c7d03a0fe32d3fda6030
Keegan Carlo Falcao <64664376+KeegTheBoi@users.noreply.github.com>
2021-03-30 08:39:19 +0200

    Add files via upload

---
+++ Classi Dell'Applicazione/Libreria per Il protocollo del prof/GestioneUtente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProtocolloGenerale
+{
+    class GestioneUtente : Operazione
+    {
+        public GestioneUtente(int codOp, string codUtente, string nomeUtente, string password, string ruolo) : base(codOp)
+        {
+            IDUtente = codUtente;
+            Nome = nomeUtente;
+            Password = password;
+            Ruolo = ruolo;
+        }
+
+        string IDUtente { get; set; }
+
+        string Nome { get; set; }
+
+        public string Password {get; set;}
+
+        string Ruolo { get; set; }
+
+
+    }
+}
---
+++ Classi Dell'Applicazione/Libreria per Il protocollo del prof/Storico.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProtocolloGenerale
+{
+    class Storico
+    {
+    }
+}
